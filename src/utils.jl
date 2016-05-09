7ab104076dfa6df23a1841293b4dc481e4818ed5
ylxdzsw <ylxdzsw@gmail.com>
2016-05-09 21:56:52 +0800

    add static server

--- src/utils.jl
+++ src/utils.jl
@@ -39,3 +39,27 @@ end
 @mixin defaultmixin begin
     :onresponse => [makeresponse, response_option, add_allow_header]
 end
+
+staticserver(dir::AbstractString="."; cache::Int=0) =
+    (r::Resource, req, id) -> begin
+        filepath = joinpath(dir, req[:path]...)
+        ext = splitext(filepath)[2][2:end]
+
+        isfile(filepath) || return Response(404)
+
+        mt = mtime(filepath) |> Dates.unix2datetime
+        mt -= Dates.Millisecond(Dates.millisecond(mt))
+
+        if "If-Modified-Since" in keys(req[:headers])
+            try # ignore any error
+                if mt <= DateTime(req[:headers]["If-Modified-Since"], Dates.RFC1123Format)
+                    return Response(304)
+                end
+            end
+        end
+
+        res = open(readbytes, filepath) |> Response
+        res.headers["Content-Type"] = get(HttpServer.mimetypes, ext, "application/octet-stream") # text/plain should be better?
+        res.headers["Last-Modified"] = Dates.format(mt, Dates.RFC1123Format)
+        res
+    end
