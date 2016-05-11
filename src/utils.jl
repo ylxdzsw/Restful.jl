b5d76760afe457db36dc9a178e3aac9f11acdbea
ylxdzsw <ylxdzsw@gmail.com>
2016-05-11 20:15:23 +0800

    add JLT: a simple template engine

--- src/utils.jl
+++ src/utils.jl
@@ -73,30 +73,52 @@ staticserver(dir::AbstractString="."; cache::Int=0) =
     end
 
 module JLT
-    cache = Dict{AbstractString, Function}
+    cache = Dict{AbstractString, Function}()
 
-    "template string -> function that turns "
+    """
+    template string -> function
+    """
     function compile(x::AbstractString)
         raw = split(x, "\$\$")
-        buf = IOBuffer(length(x.data) + length(raw)*32)
+        buf = IOBuffer(length(x.data) + length(raw)*32 + 128)
+
         iscode = false # indicating if i is code
+
+        write(buf, "(JLT_output::IO, data::Dict{Symbol, Any}) -> begin\n")
+
         for i in raw
             if iscode
-                write(buf, ' ', i, ' ')
+                write(buf, '\n', i, '\n')
             else
-                write(buf, "write(JLT_output, \"\"\"", i, "\"\"\"")
+                write(buf, "write(JLT_output, \"\"\"", i, "\"\"\")")
             end
 
-            code = !code
+            iscode = !iscode
         end
+
+        write(buf, "\nJLT_output\nend")
+
+        buf |> seekstart |> readall |> parse |> eval # eval in the JLT module
     end
 
-    "Filepath -> AbstractString"
+    "filepath -> rendered string"
     function render(file::AbstractString; kwargs...)
-
+        f = open(readall, file) |> compile
+        buf = IOBuffer()
+        f(buf, Dict{Symbol, Any}(kwargs)) |> seekstart |> readall
     end
 
-    function render_cached(file::AbstractString; kwargs...)
-
+    """
+    like render but cache the compiled function.
+    can be about 1000x faster in some cases.
+    """
+    function render_with_cache(file::AbstractString; kwargs...)
+        if haskey(cache, file)
+            buf = IOBuffer()
+            cache[file](buf, Dict{Symbol, Any}(kwargs)) |> seekstart |> readall
+        else
+            cache[file] = open(readall, file) |> compile
+            render_with_cache(file; kwargs...)
+        end
     end
 end
